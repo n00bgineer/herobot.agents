2597a573359cbd05f6dc3af746b64dff9fd38f3d
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-03-26 17:54:59 +0530

    Installing mangum and adding header

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
+import uvicorn
+from mangum import Mangum
 from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get("/")
@@ -8,3 +11,10 @@ def read_root():
 @app.get("/hello/{name}")
 def read_item(name: str):
     return {"message": f"Hello, {name}!"}
+
+# AWS LAMBDA DEPLOYMENT
+handler = Mangum(app)
+
+# LOCAL DEVELOPMENT => poetry run uvicorn index:app --reload
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
