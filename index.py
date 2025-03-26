27c9cf61773dce7db3a2ddbcbbad0b2f36de7534
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-03-26 17:31:24 +0530

    Updating server index file

--- index.py
+++ index.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-
-# Create a FastAPI instance
 app = FastAPI()
 
-# Define a GET endpoint at the root path "/"
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
-# Optional: Add another endpoint with a path parameter
 @app.get("/hello/{name}")
 def read_item(name: str):
     return {"message": f"Hello, {name}!"}
